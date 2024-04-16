4398915f0edd43d4a78d4ac4efffddb56664a1f2
luys <sanchezluys@gmail.com>
2024-04-16 14:24:08 -0500

    iniciando proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        peliculas miPelicula = new peliculas();
+        miPelicula.nombre="matrix";
+        miPelicula.duracion=180;
+        miPelicula.fechaLanzamiento=1988;
+        System.out.println("*******************************");
+        System.out.println("Mi pelicula es: "+ miPelicula.nombre);
+        System.out.println("Duracion en min es: "+ miPelicula.duracion);
+        System.out.println("Fecha de lanzamiento es: "+ miPelicula.fechaLanzamiento);
+        System.out.println("*******************************");
+
+        System.out.println("************* Impresion del objeto miPelicula ******************");
+        System.out.println(miPelicula);
+
     }
 }
---
+++ src/peliculas.java
@@ -0,0 +1,5 @@
+public class peliculas {
+    String nombre;
+    int fechaLanzamiento;
+    int duracion;
+}
