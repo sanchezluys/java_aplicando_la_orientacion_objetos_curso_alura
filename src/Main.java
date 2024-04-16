7b294fdf71f31917c341e043fd5dd9c807084757
luys <sanchezluys@gmail.com>
2024-04-16 14:29:13 -0500

    mostrando 2 clases, con su objeto

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         miPelicula.nombre="matrix";
         miPelicula.duracion=180;
         miPelicula.fechaLanzamiento=1988;
+        //**
+        peliculas otraPelicula = new peliculas();
+        otraPelicula.nombre="incepcion";
+        otraPelicula.duracion=120;
+        otraPelicula.fechaLanzamiento=1998;
+        //**
         System.out.println("*******************************");
         System.out.println("Mi pelicula es: "+ miPelicula.nombre);
         System.out.println("Duracion en min es: "+ miPelicula.duracion);
@@ -12,6 +18,14 @@ public class Main {
 
         System.out.println("************* Impresion del objeto miPelicula ******************");
         System.out.println(miPelicula);
+        //**
+        System.out.println("*******************************");
+        System.out.println("Mi otra pelicula es: "+ otraPelicula.nombre);
+        System.out.println("Duracion en min es: "+ otraPelicula.duracion);
+        System.out.println("Fecha de lanzamiento es: "+ otraPelicula.fechaLanzamiento);
+        System.out.println("*******************************");
 
+        System.out.println("************* Impresion del objeto otraPelicula ******************");
+        System.out.println(otraPelicula);
     }
 }
