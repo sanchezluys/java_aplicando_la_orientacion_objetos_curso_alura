7d18b4dcafc869b393633fa7c821008430efc0da
luys <sanchezluys@gmail.com>
2024-04-18 11:08:46 -0500

    ahora usando setter, getter

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,27 @@ import com.aluracursos.modelos.peliculas;
 public class Main {
     public static void main(String[] args) {
         peliculas miPelicula = new peliculas();
-        miPelicula.nombre="matrix";
-        miPelicula.duracion=180;
-        miPelicula.fechaLanzamiento=1988;
+        miPelicula.setNombre("matrix");
+        miPelicula.setDuracion(180);
+        miPelicula.setFechaLanzamiento(1988);
         //**
         peliculas otraPelicula = new peliculas();
-        otraPelicula.nombre="incepcion";
-        otraPelicula.duracion=120;
-        otraPelicula.fechaLanzamiento=1998;
+        otraPelicula.setNombre("incepcion");
+        otraPelicula.setFechaLanzamiento(1998);
         //**
         System.out.println("*******************************");
-        System.out.println("Mi pelicula es: "+ miPelicula.nombre);
-        System.out.println("Duracion en min es: "+ miPelicula.duracion);
-        System.out.println("Fecha de lanzamiento es: "+ miPelicula.fechaLanzamiento);
+        System.out.println("Mi pelicula es: "+ miPelicula.getNombre());
+        System.out.println("Duracion en min es: "+ miPelicula.getDuracion());
+        System.out.println("Fecha de lanzamiento es: "+ miPelicula.getFechaLanzamiento());
         System.out.println("*******************************");
 
         System.out.println("************* Impresion del objeto miPelicula ******************");
         System.out.println(miPelicula);
         //**
         System.out.println("*******************************");
-        System.out.println("Mi otra pelicula es: "+ otraPelicula.nombre);
-        System.out.println("Duracion en min es: "+ otraPelicula.duracion);
-        System.out.println("Fecha de lanzamiento es: "+ otraPelicula.fechaLanzamiento);
+        System.out.println("Mi otra pelicula es: "+ otraPelicula.getNombre());
+        System.out.println("Duracion en min es: "+ otraPelicula.getDuracion());
+        System.out.println("Fecha de lanzamiento es: "+ otraPelicula.getFechaLanzamiento());
         System.out.println("*******************************");
         //**
         System.out.println("************* Impresion del objeto otraPelicula ******************");
--- src/com/aluracursos/modelos/peliculas.java
+++ src/com/aluracursos/modelos/peliculas.java
@@ -1,11 +1,37 @@
 package com.aluracursos.modelos;
 
 public class peliculas {
-    String nombre;
-    int fechaLanzamiento;
-    int duracion;
+    private String nombre;
+    private int fechaLanzamiento;
+    private int duracion;
     //**
-    void muestraFicha() {
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(int fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public void muestraFicha() {
         System.out.println("*******************************");
         System.out.println("Mi pelicula es: "+ nombre);
         System.out.println("Duracion en min es: "+ duracion);
